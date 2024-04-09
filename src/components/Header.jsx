6025e83a17a1de46397d38d39e5620c6ff2bc605
JAYASHRI <141231703+jayashri16@users.noreply.github.com>
2024-04-09 23:13:20 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/investment-calculator-logo.png";
+
+export default function Header() {
+  return (
+    <header id ="header">
+        
+      <img src={logo} alt=" logo showing a money bag" />
+      <h1>Systematic Investment Plan </h1>
+    </header>
+  );
+}
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+export default function UserInput({ onChange, userInput }) {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Inverstment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              onChange("initialInvestment", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>annualInvestment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              onChange("annualInvestment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected returns</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) => onChange("expectedReturn", event.target.value)}
+          />
+        </p>
+        <p>
+          <label>Time period</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => onChange("duration", event.target.value)}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
